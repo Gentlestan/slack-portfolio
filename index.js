c30b88d824a0ad2a7edbd8b36e7801bf707f4392
Gentlestan <81222819+Gentlestan@users.noreply.github.com>
2023-09-07 00:05:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function updateDayAndTime(){
+    const dayOfWeekElement = document.querySelector('[data-testid= "currentDayOfTheWeek"]');
+    const currentTimeElement = document.querySelector('[data-testid="currentUTCTime"]')
+
+    const now = new Date();
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const dayOfWeek = daysOfWeek[now.getDay()];
+    const time = now.toLocaleTimeString();
+
+    dayOfWeekElement.textContent = `Today is ${dayOfWeek}`;
+    currentTimeElement.textContent = `Current time is ${time}`
+}
+
+setInterval(updateDayAndTime, 1000);
+
+// Initial update
+updateDayAndTime()
